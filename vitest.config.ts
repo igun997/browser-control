a4f8ee9d53d79084ebb8f11ef2a71bef6f54aea1
Indra Gunanda <indra.gunanda@gmail.com>
2026-05-19 22:30:39 +0700

    chore: scaffold server package

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['extension/src/test/setup.ts']
+    setupFiles: ['extension/src/test/setup.ts'],
+    include: ['extension/src/**/*.test.ts', 'server/src/**/*.test.ts']
   }
 });
